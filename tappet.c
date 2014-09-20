94b57e920a9e5b773219ba11b23edbc044584736
Abhijit Menon-Sen <ams@toroid.org>
2014-09-21 02:02:46 +0530

    Move interface code out of main into tap_attach
    It's best to keep main on a strict diet right from the start.

--- tappet.c
+++ tappet.c
@@ -18,10 +18,11 @@
 
 #include "tweetnacl.h"
 
+int tap_attach(const char *name);
+
 int main(int argc, char *argv[])
 {
-    int n, fd;
-    struct ifreq ifr;
+    int fd;
 
     /*
      * We require exactly five arguments: the interface name, the name
@@ -36,18 +37,38 @@ int main(int argc, char *argv[])
     }
 
     /*
-     * We attach to the TAP interface whose name is given as our first
-     * argument (as described in Documentation/networking/tuntap.txt).
-     * If this code is run as root, it will create the interface. (It
-     * would be nice to report a more useful error when the interface
-     * doesn't exist, but TUNGETIFF works on the attached fd; we have
-     * only an interface name.)
+     * Attach to the given TAP interface as an ordinary user (so that we
+     * don't create it by mistake; we assume it's already configured).
      */
 
     if (geteuid() == 0) {
         fprintf(stderr, "Please run tappet as an ordinary user\n");
         return -1;
     }
+
+    fd = tap_attach(argv[1]);
+    if (fd < 0)
+        return -1;
+
+    /* … */
+
+    return 0;
+}
+
+/*
+ * Attaches to the TAP interface with the given name and returns an fd
+ * (as described in linux/Documentation/networking/tuntap.txt).
+ *
+ * If this code is run as root, it will create the interface if it does
+ * not exist. (It would be nice to report a more useful error when the
+ * interface doesn't exist, but TUNGETIFF works on the attached fd; we
+ * have only an interface name.)
+ */
+
+int tap_attach(const char *name)
+{
+    int n, fd;
+    struct ifreq ifr;
 
     fd = open("/dev/net/tun", O_RDWR);
     if (fd < 0) {
@@ -56,17 +77,15 @@ int main(int argc, char *argv[])
     }
 
     memset(&ifr, 0, sizeof(ifr));
-    strncpy(ifr.ifr_name, argv[1], IFNAMSIZ);
+    strncpy(ifr.ifr_name, name, IFNAMSIZ);
     ifr.ifr_flags = IFF_TAP;
 
     n = ioctl(fd, TUNSETIFF, (void *) &ifr);
     if (n < 0) {
-        fprintf(stderr, "Couldn't attach to %s: %s\n", argv[1], strerror(errno));
+        fprintf(stderr, "Couldn't attach to %s: %s\n", name, strerror(errno));
         close(fd);
         return -1;
     }
 
-    /* … */
-
-    return 0;
+    return fd;
 }
